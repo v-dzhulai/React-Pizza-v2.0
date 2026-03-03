b79e7ab252b05cd1299d1158062737f037cc2788
Viktor Dzhulai <dzulajvita@gmail.com>
2026-03-03 12:56:43 +0200

    added components: header, categories, sort, pizza-block

---
+++ src/components/Cetegories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Всі</li>
+        <li>М'ясні</li>
+        <li>Веґетаріанська</li>
+        <li>Ґриль</li>
+        <li>Гострі</li>
+        <li>Закриті</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
